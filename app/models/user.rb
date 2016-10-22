ddbe0de262ce78602dda536dd3524704d1cc0516
Matheus Bras <bras.matheus@gmail.com>
2016-10-22 11:19:39 -0200

    Add shouts association on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :shouts
+  has_many :sent_shouts, class_name: 'Shout', foreign_key: :shouter_id
+
   validates :name, presence: true
   validates :nickname, presence: true, uniqueness: true
   validates :profile_image_url, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'associations' do
+    it { is_expected.to have_many(:shouts) }
+    it { is_expected.to have_many(:sent_shouts).class_name('Shout').with_foreign_key(:shouter_id) }
+  end
+
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:nickname) }
