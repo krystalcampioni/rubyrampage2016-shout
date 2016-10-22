4c8c08555c6be3a923331072a2226e3ad0be422b
Matheus Bras <bras.matheus@gmail.com>
2016-10-22 11:43:49 -0200

    Fix user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     name 'Nome'
-    nickname "nickname_#{SecureRandom.hex(2)}"
-    profile_image_url "http://placehold.it/200/200"
+    sequence(:nickname) { |n| "nickname_#{n}" }
+    profile_image_url 'http://placehold.it/200/200'
   end
 end
