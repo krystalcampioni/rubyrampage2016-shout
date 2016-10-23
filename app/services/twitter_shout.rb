3e2fbda52b928f57abe1a8102eeff5c2edae891c
Matheus Bras <bras.matheus@gmail.com>
2016-10-23 21:21:42 -0200

    Tested

--- app/services/twitter_shout.rb
+++ app/services/twitter_shout.rb
@@ -4,25 +4,21 @@ class TwitterShout
   end
 
   def create
-    begin
-      @shout.users.each do |user|
-        client.update(message_for_user(user))
-      end
-    rescue
-      nil
+    return if Rails.env.test?
+
+    @shout.users.each do |user|
+      client.update(message_for_user(user))
     end
   end
 
   def message_for_user(user)
-    shouter = @shout.shouter.nickname
-    nickname = user.nickname
-    app_url = "http://#{Rails.application.secrets.domain}/u/#{nickname}"
-    "@#{nickname} Hey, @#{shouter_nickname} just sent you a shout at #{app_url}"
+    "@#{user.nickname} Hey, @#{@shout.shouter.nickname} just sent you a shout at http://#{Rails.application.secrets.domain}/u/#{user.nickname}"
   end
 
   private
 
   def client
+
     @client ||= Twitter::REST::Client.new do |config|
       config.consumer_key = Rails.application.secrets.shout_twitter_api_key
       config.consumer_secret = Rails.application.secrets.shout_twitter_secret
