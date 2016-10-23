8c91513474458acf10c418432ee96888da205c40
Pedro Nascimento <pnascimento@gmail.com>
2016-10-23 13:01:48 -0200

    Use case insensitive nicknames when looking for users.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:nickname) }
-    it { is_expected.to validate_uniqueness_of(:nickname) }
+    it { is_expected.to validate_uniqueness_of(:nickname).case_insensitive }
     it { is_expected.to validate_presence_of(:profile_image_url) }
   end
 
